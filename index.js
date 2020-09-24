81e15a065c3c246095290f5ef9fa5ccc0265be09
Paranoid-AF <7659335+Paranoid-AF@users.noreply.github.com>
2020-09-24 15:13:12 +0800

    implement task handler

--- index.js
+++ index.js
@@ -33,26 +33,40 @@ chokidar.watch(configPaths.serviceStore).on('change', (filepath) => {
   const serviceName = file.substring(0, file.length - 5)
   const changeType = file.substring(file.length - 4, file.length) // rslt or task
   const targetService = services.find(val => val.name === serviceName)
-  if(targetService !== undefined){
+  if(typeof targetService === 'object'){
     fs.readFile(path.resolve(filepath), { encoding: 'utf8' }, (err, data) => {
       if(!err && data !== ''){
-          fs.writeFileSync(filepath, '', { encoding: 'utf8'})
-          if(changeType === "rslt"){
-            data.split('\n').forEach(val => {
-              if(val !== ''){
-                const info = serializer.deserialize(val)
-                console.log(info)
+        fs.writeFileSync(filepath, '', { encoding: 'utf8'})
+        if(changeType === "task"){
+          data.split('\n').forEach(val => {
+            if(val !== ''){
+              const info = serializer.deserialize(val)
+              const method = targetService.actions[info.action]
+              if(typeof method === "function"){
+                const actionPayload = method(info)
+                const result = {
+                  uuid: info.uuid,
+                  resolver: serviceName,
+                  action: info.action,
+                  time: Math.round((new Date()).getTime() / 1000).toString(),
+                  timeIssued: info.time,
+                  payload: actionPayload,
+                  ref: info.payload,
+                }
+                const resultSerialized = serializer.serialize(result)
+                fs.writeFile(path.resolve(configPaths.serviceStore, `./${info.issuer}.rslt`), resultSerialized + '\n', { encoding: 'utf8', flag: 'a'}, () => {})
               }
-            })
-          }
-          if(changeType === "task"){
-            data.split('\n').forEach(val => {
-              if(val !== ''){
-                const info = serializer.deserialize(val)
-                console.log(info)
-              }
-            })
-          }
+            }
+          })
+        }
+        if(changeType === "rslt"){
+          data.split('\n').forEach(val => {
+            if(val !== ''){
+              const info = serializer.deserialize(val)
+              console.log(info)
+            }
+          })
+        }
       }
     })
   }
--- services/demo.js
+++ services/demo.js
@@ -4,8 +4,9 @@ module.exports = {
     query: (data) => {
 
     },
-    connect: (data) => {
-      
+    play_with_me: (data) => {
+      console.log(data.payload)
+      return "world.execute(me);"
     }
   },
 }
